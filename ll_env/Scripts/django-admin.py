60064ef28acd8e230df6ded6855b4edb411fa860
Clebson <clebson2007.farias@gmail.com>
2016-09-15 11:54:53 -0300

    Inicio

---
+++ ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Clebson Doc\PIBIC 2016-17\Django\learning_log\ll_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
