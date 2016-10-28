537c2270786fc5e04f2a90aac2ebd3ee8faddd47
Clebsonpy <Clebson2007.farias@gmail.com>
2016-10-28 00:45:31 -0300

    ready for deployment.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -132,3 +132,20 @@ LOGIN_URL = '/users/login/'
 BOOTSTRAP3 = {
     'include_jquery': True,
 }
+
+#Configuração para o Heroku
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+        dj_database_url.config(default='postgres://localhost')
+    }
+
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    ALLOWED_HOSTS = ['*']
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
